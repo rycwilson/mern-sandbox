94a3d809ca9f038ac9ae015e717670a68d4fe62a
Ryan Wilson <rycwilson@gmail.com>
2023-08-28 15:30:42 -0700

    adds notFound middleware

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import dotenv from 'dotenv';
+
 import connectDb from './config/db.js';
+
+import notFound from './middleware/not-found.js';
 
 const appName = 'node-api';
 const app = express();
@@ -10,6 +13,7 @@ dotenv.config();
 app
   .get('/', (req, res) => res.send('node api'))   // just a sanity check
   .use(express.json())
+  .use(notFound);
 
 connectDb(`${process.env.MONGO_URI}/${appName}`)
   .then(start)
