1de775f1deec7d1c2793f8abce3cc985dc90084b
Ryan Wilson <rycwilson@gmail.com>
2023-09-03 10:45:17 -0700

    adds interface extensions to express Request type

--- global.d.ts
+++ global.d.ts
@@ -1,9 +1,28 @@
 // https://dev.to/asjadanis/parsing-env-with-typescript-3jjm
-namespace NodeJS {
-  interface ProcessEnv {
-    PORT: string,
-    MONGO_URI: string,
-    JWT_SECRET: string,
-    JWT_EXPIRES_IN: string
+
+import { Request } from 'express';
+
+type RegisteredUser = { id: string, name: string }
+
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      PORT: string,
+      MONGO_URI: string,
+      JWT_SECRET: string,
+      JWT_EXPIRES_IN: string
+    }
+  }
+
+  namespace Express {
+    interface Request { 
+      user?: RegisteredUser 
+    }
+  }
+
+  interface AuthenticatedRequest extends Request {
+    user: RegisteredUser
   }
 }
+
+export {}
