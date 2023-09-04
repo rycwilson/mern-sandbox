0ed623a5c2a5eaa5464e29291aef301d5f21770f
Ryan Wilson <rycwilson@gmail.com>
2023-09-04 16:20:21 -0700

    adds type assertions to route handlers

--- routes/widgets.ts
+++ routes/widgets.ts
@@ -1,15 +1,18 @@
+import type { RequestHandler } from 'express';
 import express from 'express';
 import { getAllWidgets, createWidget, getWidget, updateWidget, deleteWidget } from '../controllers/widgets.js';
 
 const router = express.Router()
 
+// RequestHandler assertion is necessary due to argument being AuthenticatedRequest instead of Request
+// TODO: probably a better way to do this
 router.route('/')
-  .get(getAllWidgets)
-  .post(createWidget);
+  .get(getAllWidgets as RequestHandler)
+  .post(createWidget as RequestHandler);
   
 router.route('/:id')
-  .get(getWidget)
-  .put(updateWidget)
-  .delete(deleteWidget);
+  .get(getWidget as RequestHandler)
+  .put(updateWidget as RequestHandler)
+  .delete(deleteWidget as RequestHandler);
 
 export default router;
