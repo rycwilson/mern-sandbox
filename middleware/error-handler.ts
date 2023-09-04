efdc6f761e8dfc9543eeed9b5d2ec83ca5a9adbc
Ryan Wilson <rycwilson@gmail.com>
2023-09-04 16:21:06 -0700

    adds error types for mongoose and mongodb

--- middleware/error-handler.ts
+++ middleware/error-handler.ts
@@ -1,23 +1,28 @@
-import { Request, Response, NextFunction } from 'express';
+import type { Request, Response, NextFunction } from 'express';
+import { Error as MongooseError } from 'mongoose';
+import { MongoError } from 'mongodb';
 import { StatusCodes } from 'http-status-codes';
 import { CustomApiError } from '../errors/index.js';
 
-const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
-  const customError = (err instanceof CustomApiError) && {
-    statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
-    msg: err.message || 'Sorry, there was an error'
+type ErrorType = Error | MongooseError | MongoError | CustomApiError;
+
+const errorHandler = (err: ErrorType, req: Request, res: Response, next: NextFunction) => {
+  const customError = {
+    statusCode: (err instanceof CustomApiError && err.statusCode) || StatusCodes.INTERNAL_SERVER_ERROR,
+    msg: (err instanceof CustomApiError && err.message) || 'Sorry, there was an error'
   }
-  if (err.name === 'ValidationError') {
+  if (err instanceof MongooseError.ValidationError) {
     console.log(Object.values(err.errors).map(item => item.message).join(','))
     customError.msg = Object.values(err.errors).map(item => item.message).join(', ');
     customError.statusCode = 400;
   }
-  if (err.name === 'CastError') {
+  if (err instanceof MongooseError.CastError) {
     customError.msg = `No record found with id: ${err.value}`;
     customError.statusCode = 404;
   }
-  if (err.code && err.code === 11000) {
-    customError.msg = `That ${Object.keys(err.keyValue)} already exists`;
+  if (err instanceof MongoError && err.code === 11000) {
+    // customError.msg = `That ${Object.keys(err.keyValue)} already exists`;
+    customError.msg = 'Duplicate field'
     customError.statusCode = 400;
   }
   // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
