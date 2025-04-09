81908802fd2ea6b5c3fb2edb9c61098ec38364fc
Ryan Wilson <rycwilson@gmail.com>
2025-04-08 17:21:13 -0700

    adds a custom event emitter

--- middleware/misc.ts
+++ middleware/misc.ts
@@ -2,6 +2,13 @@ import type { Request, Response, NextFunction } from 'express';
 // import { writeFile } from 'fs';
 // import { promisify } from 'util';
 import { promises as fsPromises } from 'fs';
+import EventEmitter from 'events';
+
+const customEmitter = new EventEmitter();
+
+customEmitter.on('createdWidget', () => {
+  console.log('widget was created');
+})
 
 export const logCreatedWidget = (req: Request, res: Response, next: NextFunction) => {
   // const writeFilePromise = promisify(writeFile);
@@ -9,7 +16,10 @@ export const logCreatedWidget = (req: Request, res: Response, next: NextFunction
   const logData = `Created Widget @ ${new Date().toLocaleString()}: ${JSON.stringify(req.body)}\n`;
   // writeFilePromise('./logs/log.txt', logData, { encoding: 'utf8', flag: 'a' })
   writeFile('./logs/log.txt', logData, { encoding: 'utf8', flag: 'a' })
-    .then(() => console.log('Log written successfully'))
+    .then(() => {
+      console.log('widget was logged')
+      customEmitter.emit('createdWidget');
+    })
     .catch((err) => console.error('Error writing log:', err));
 
   next();
