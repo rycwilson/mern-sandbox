41234f8cc184f54dad7ca9ad53cd3573d4048709
Ryan Wilson <rycwilson@gmail.com>
2025-04-20 15:45:19 -0700

    refactors login request handler

--- controllers/authController.ts
+++ controllers/authController.ts
@@ -1,7 +1,7 @@
 import type { Request, Response } from 'express';
 import User from '../models/user.ts';
 import { StatusCodes } from 'http-status-codes';
-import { BadRequestError, UnauthenticatedError } from '../errors/custom-errors.ts';
+import { UnauthenticatedError } from '../errors/custom-errors.ts';
 
 export async function register(req: Request, res: Response) {
   // mongoose will validate
@@ -9,21 +9,18 @@ export async function register(req: Request, res: Response) {
   // if (!name || !email || !password) {
   //   throw new BadRequestError('Name, email, and password are required')
   // }
-
+  const isFirstAccount = await User.countDocuments() === 0;
+  req.body.role = isFirstAccount ? 'admin' : 'user';
   const user = await User.create(req.body);
   res.status(StatusCodes.CREATED).json({ user: { name: user.fullName }, token: user.createJWT() });
 };
 
 export async function login(req: Request, res: Response) {
-  const { email, password } = req.body;
-  if (!email || !password) throw new BadRequestError('Email and password are required');
-  const user = await User.findOne({ email });
-  if (!user) throw new UnauthenticatedError('Invalid credentials');
-  const passwordIsCorrect = await user.comparePassword(password);
-  if (passwordIsCorrect) {
+  const user = await User.findOne({ email: req.body.email });
+  const validCredentials = user && await user.comparePassword(req.body.password);
+  if (validCredentials) {
     return res.status(StatusCodes.OK).json({ user: { name: user.fullName }, token: user.createJWT() });
   } else {
-    console.log('invalid credentials')
     throw new UnauthenticatedError('Invalid credentials');
   }
 };
