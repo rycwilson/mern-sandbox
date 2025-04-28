e9b3918f46a6ded0409085d89063f9819d9a2b60
Ryan Wilson <rycwilson@gmail.com>
2025-04-28 16:33:43 -0700

    adds Login component

--- vite-react-ts/src/pages/Login.tsx
+++ vite-react-ts/src/pages/Login.tsx
@@ -1,13 +1,28 @@
-import { Link } from 'react-router';
+import { Form, useNavigation, Link, useActionData } from 'react-router';
+import FormRow from '../components/FormRow.tsx';
 import Wrapper from '../assets/wrappers/register-and-login.ts';
 
-function Login() {
+export default function Login() {
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === 'submitting';
+
+  // simple validation with useActionData
+  const errors = useActionData();
+
   return (
     <Wrapper>
-      <h1>Login</h1>
-      <Link to="/register">Register</Link>
+      <Form method="post" className="form" noValidate>
+        <h4>Login</h4>
+        {errors?.msg ?
+          <p style={{color:'red'}}>{errors.msg}</p> :
+          <p>Don't have an account?&nbsp;&nbsp;<Link to="/register">Register</Link></p>
+        }
+        <FormRow type="email" name="email" labelText="Email" autoComplete="email" required />
+        <FormRow type="password" name="password" labelText="Password" autoComplete="new-password" required/>
+        <button className="btn btn-block" disabled={isSubmitting}>
+          {isSubmitting ? 'Submitting...' :  'Login'}
+        </button>
+      </Form>
     </Wrapper>
   )
 }
-
-export default Login;
