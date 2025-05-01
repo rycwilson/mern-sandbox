490953a6530389af7f97f7834f04f0d00a3d7e59
Ryan Wilson <rycwilson@gmail.com>
2025-04-30 18:13:25 -0700

    adds Modal to DashboardLayout

--- vite-react-ts/src/pages/DashboardLayout.tsx
+++ vite-react-ts/src/pages/DashboardLayout.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { useLoaderData, useNavigate } from 'react-router';
 import { Outlet } from 'react-router';
 import Wrapper from '../assets/wrappers/dashboard';
-import { BigSidebar, Navbar, SmallSidebar } from '../components';
+import { BigSidebar, Navbar, SmallSidebar, Modal } from '../components';
 import { DashboardContext } from '../contexts';
 import { toast } from 'react-toastify';
 
@@ -10,10 +10,16 @@ export default function DashboardLayout() {
   const user = useLoaderData();
   const navigate = useNavigate();
   const [showSidebar, setShowSidebar] = useState(false);
+  const [modal, setModal] = useState<{ open: boolean; content: ModalContent }>({ open: false, content: null });
 
   const toggleSidebar = () => {
     setShowSidebar(!showSidebar);
   };
+
+  const toggleModal = (content: ModalContent = null) => {
+    console.log('toggling modal', content);
+    setModal({ open: !modal.open, content });
+  }
 
   const logoutUser = async () => {
     navigate('/');
@@ -21,8 +27,13 @@ export default function DashboardLayout() {
     toast.success('Logged out successfully');
   }
 
+  const handleModalClosed = () => {
+    console.log('modal did close')
+  };
+
   return (
-    <DashboardContext.Provider value={{ user, showSidebar, toggleSidebar, logoutUser }}>
+    <DashboardContext.Provider value={{ user, showSidebar, toggleSidebar, toggleModal, logoutUser }}>
+      <Modal isOpen={modal.open} content={modal.content} onClose={handleModalClosed}></Modal>
       <Wrapper>
         <main className="dashboard">
           <SmallSidebar />
